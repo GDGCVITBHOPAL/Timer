be144d8438d6649f3fb9cc42fb6dcbafded69618
Alok Mathur <73957024+alok27a@users.noreply.github.com>
2021-10-02 22:50:47 +0530

    Splash Screen Added with Animation
    Splash screen added with a beautiful lottie animation

---
+++ app/src/main/java/com/example/timer/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.example.timer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(MainActivity2.this, MainActivity.class));
+            finish();
+        },2100);
+    }
+}
