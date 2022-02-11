81b1ac71bff7adc95c19c776f7a27bb12713a2ac
Harsh Raj <harsh.raj8833@gmail.com>
2022-02-11 09:58:33 +0530

    Button Icon

--- app/src/main/java/com/example/timer/MainActivity.kt
+++ app/src/main/java/com/example/timer/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Stop
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
@@ -16,6 +20,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PointMode
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.onSizeChanged
@@ -83,38 +88,43 @@ fun Timer(
             value = currentTime / totalTime.toFloat()
         }
     }
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = modifier
-            .onSizeChanged {
-                size = it
+    Column() {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = modifier
+                .onSizeChanged {
+                    size = it
+                }
+        ) {
+            Canvas(modifier = modifier) {
+                drawArc(
+                    color = inactiveBarColor,
+                    startAngle = 360f,
+                    sweepAngle = 360f,
+                    useCenter = false,
+                    size = Size(size.width.toFloat(), size.height.toFloat()),
+                    style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+                )
+                drawArc(
+                    color = activeBarColor,
+                    startAngle = 360f,
+                    sweepAngle = 360f * value,
+                    useCenter = false,
+                    size = Size(size.width.toFloat(), size.height.toFloat()),
+                    style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+                )
             }
-    ) {
-        Canvas(modifier = modifier) {
-            drawArc(
-                color = inactiveBarColor,
-                startAngle = 360f,
-                sweepAngle = 360f,
-                useCenter = false,
-                size = Size(size.width.toFloat(), size.height.toFloat()),
-                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
-            )
-            drawArc(
-                color = activeBarColor,
-                startAngle = 360f,
-                sweepAngle = 360f * value,
-                useCenter = false,
-                size = Size(size.width.toFloat(), size.height.toFloat()),
-                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            Text(
+                text = (currentTime / 1000L).toString(),
+                fontSize = 44.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
             )
         }
-        Text(
-            text = (currentTime / 1000L).toString(),
-            fontSize = 44.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.White
-        )
         Button(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(40.dp),
             onClick = {
                 if(currentTime <= 0L) {
                     currentTime = totalTime
@@ -123,7 +133,7 @@ fun Timer(
                     isTimerRunning = !isTimerRunning
                 }
             },
-            modifier = Modifier.align(Alignment.BottomCenter),
+            shape = CircleShape,
             colors = ButtonDefaults.buttonColors(
                 backgroundColor = if (!isTimerRunning || currentTime <= 0L) {
                     Color.Green
@@ -132,13 +142,18 @@ fun Timer(
                 }
             )
         ) {
-            Text(
-                text = if (isTimerRunning && currentTime > 0L) "Stop"
-                else if (!isTimerRunning && currentTime >= 0L) "Start"
+            Icon(
+                imageVector = if (isTimerRunning && currentTime > 0L) Icons.Filled.Pause
+                else if (!isTimerRunning && currentTime >= 0L) Icons.Filled.PlayArrow
+                else Icons.Filled.Refresh,
+                contentDescription = if (isTimerRunning && currentTime > 0L) "Pause"
+                else if (!isTimerRunning && currentTime >= 0L) "Play"
                 else "Restart"
             )
         }
+
     }
+
 }
 
 @Preview("Main Screen")
@@ -152,7 +167,7 @@ fun preview_Timer(){
             contentAlignment = Alignment.Center
         ) {
             Timer(
-                totalTime = 10L * 1000L,
+                totalTime = 100L * 1000L,
                 handleColor = Color.Green,
                 inactiveBarColor = Color.DarkGray,
                 activeBarColor = Color(0xFFFF4C29),
